d4fe3ac20aa529540818cb43757df522da59d146
corbinedwards <corbinedwards@comcast.net>
2022-02-24 13:27:54 -0600

    use process.env.PORT

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const lang = require('./languages.json');
-const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -10,4 +11,4 @@ app.use(express.json());
 app.get('/language', (req,res) => res.json(lang));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, '/index.html')));
 
-app.listen(port, () => console.log(`Listening on http://localhost:3000/`));
+app.listen(process.env.PORT, () => console.log(`Listening...`));
